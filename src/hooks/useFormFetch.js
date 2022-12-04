c1ff158ee882cd3ddf648b4d1ca70dd14940666f
Richard Perdomo <richard.perdomo121@gmail.com>
2022-12-04 17:46:41 +0200

    removed not used imports

--- src/hooks/useFormFetch.js
+++ src/hooks/useFormFetch.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useToast } from '../context/ToastContext';
 import { useUser } from '../context/UserContext';
 import { baseURL } from '../utils/constants';
